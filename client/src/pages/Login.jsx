75298bdacaee0d9f1e16a954031f4b77fbf79e6f
RuslanGon <ruslan441006@gmail.com>
2025-06-02 18:33:39 +0300

    wer

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import {useForm} from 'react-hook-form'
 import { useNavigate } from 'react-router-dom'
+import "../App.css";
 
 const Login = () => {
 const user = ''
@@ -17,10 +18,13 @@ useEffect(() => {
 <div className='h-full w-full lg:w-2/3 flex flex-col items-center justify-center'>
   <div className='w-full md:max-w-lg 2xl:max-w-3xl flex flex-col items-center justify-center gap-5 md:gap-y-10 2xl:-mt-20'>
     <span className='flex gap-1 py-1 px-3 border rounded-full text-sm md:text-base border-gray-300 text-gray-600'>Manage your task in one place!</span>
-    <p className='flex flex-col gap-0 md:gap-4 text-4xl md:text-6xl 2xl:text-7xl'>
+    <p className='flex flex-col gap-0 md:gap-4 text-4xl md:text-6xl 2xl:text-7xl font-black text-center text-blue-700'>
       <span>Cloud-Based</span>
       <span>Task Manager </span>     
     </p>
+    <div className='cell'>
+      <div className='circle rotate-in-up-left'></div>
+    </div>
   </div>
 </div>
 <div></div>
