0cfe7dcf2ed34b3f59cb37625de943c98ceda473
RuslanGon <ruslan441006@gmail.com>
2025-06-02 11:53:40 +0300

    wer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,33 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Login from './pages/Login.jsx';
-import Layout from './pages/Layout.jsx';
-import Dashboard from './pages/Dashboard.jsx';
-import Tasks from './pages/Tasks.jsx';
-import Users from './pages/Users.jsx';
-import Trash from './pages/Trash.jsx';
-import TaskDetails from './pages/TaskDetails.jsx';
+import { Navigate, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Login from "./pages/Login.jsx";
+import Layout from "./pages/Layout.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import Tasks from "./pages/Tasks.jsx";
+import Users from "./pages/Users.jsx";
+import Trash from "./pages/Trash.jsx";
+import TaskDetails from "./pages/TaskDetails.jsx";
+import { Toaster } from 'sonner';
 
 function App() {
   return (
     <main className="w-full min-h-screen bg-[#f3f4f6]">
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route path='/' element={<Navigate to="/dashboard" />} />
-          <Route path='/dashboard' element={<Dashboard />}></Route>
-          <Route path='/tasks' element={<Tasks />}></Route>
-          <Route path='/completed/:status' element={<Tasks />}></Route>
-          <Route path='/in-progress/:status' element={<Tasks />}></Route>
-          <Route path='/todo/:status' element={<Tasks />}></Route>
-          <Route path='/team' element={<Users/>}></Route>
-          <Route path='/trashed' element={<Trash/>}></Route>
-          <Route path='/task/:id' element={<TaskDetails/>}></Route>
+          <Route path="/" element={<Navigate to="/dashboard" />} />
+          <Route path="/dashboard" element={<Dashboard />}></Route>
+          <Route path="/tasks" element={<Tasks />}></Route>
+          <Route path="/completed/:status" element={<Tasks />}></Route>
+          <Route path="/in-progress/:status" element={<Tasks />}></Route>
+          <Route path="/todo/:status" element={<Tasks />}></Route>
+          <Route path="/team" element={<Users />}></Route>
+          <Route path="/trashed" element={<Trash />}></Route>
+          <Route path="/task/:id" element={<TaskDetails />}></Route>
         </Route>
+        <Route />
+        <Route path="/log-in" element={<Login />}></Route>
       </Routes>
+      <Toaster />
     </main>
   );
 }
