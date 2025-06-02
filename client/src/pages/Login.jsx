3d2c5f9bb046898e1cf25f40dce8156d2907b724
RuslanGon <ruslan441006@gmail.com>
2025-06-02 12:28:38 +0300

    wr

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import {useForm} from 'react-hook-form'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
+const user = ''
+const { register, handleSubmit, formState: {errors}} = useForm()   
+const navigate = useNavigate()
+
+useEffect(() => {
+    user && navigate('/dashboard')
+}, [navigate, user])
   return (
   <div>Login</div>
   )
