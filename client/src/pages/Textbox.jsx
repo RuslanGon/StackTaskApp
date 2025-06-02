ca953be703921d5be95325a957ab9f16da7b6c61
RuslanGon <ruslan441006@gmail.com>
2025-06-02 22:59:39 +0300

    wrer

--- client/src/pages/Textbox.jsx
+++ client/src/pages/Textbox.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import clsx from 'clsx'
 
-const Textbox = () => {
-  return (
-    <div>Textbox</div>
-  )
-}
+const Textbox = React.forwardRef(
+  ({ type, placeholder, label, className, register, name, error }, ref) => {
+    return (
+      <div className="w-full flex flex-col gap-1">
+        {label && <label htmlFor={name} className="text-slate-800 "></label>}
+        <div>
+          <input type={type} name={name} placeholder={placeholder} ref={ref} {...register} aria-invalid={error ? 'true' : 'false'}
+          className={clsx('', className)} />
+        </div>
+      </div>
+    );
+  }
+);
 
 export default Textbox
