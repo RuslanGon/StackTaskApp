851abcc9101af39eed8c278e658d1c88522a353e
RuslanGon <ruslan441006@gmail.com>
2025-06-02 12:18:40 +0300

    wer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Navigate, Outlet, Route, Routes, useLocation } from "react-router-dom";
 import "./App.css";
 import Login from "./pages/Login.jsx";
-import Layout from "./pages/Layout.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
 import Tasks from "./pages/Tasks.jsx";
 import Users from "./pages/Users.jsx";
@@ -10,6 +9,28 @@ import TaskDetails from "./pages/TaskDetails.jsx";
 import { Toaster } from 'sonner';
 
 function App() {
+
+function Layout() {
+  const user = "";
+  const location = useLocation();
+  return user ? (
+    <div className="w-full h-screen flex flex-col md:flex-row">
+      <div className="w-1/5 h-screen bg-white sticky top-0 hidden md:block">
+        {/* <Sidebar /> */}
+      </div>
+      {/* <MobileSidebar /> */}
+      <div className="flex-1 overflow-y-auto">
+        {/* <Navbar /> */}
+        <div className="p-4 2xl:px-10">
+          <Outlet />
+        </div>
+        </div>
+    </div>
+  ) : (
+    <Navigate to="log-in" state={{ from: location }} replace />
+  );
+}  
+
   return (
     <main className="w-full min-h-screen bg-[#f3f4f6]">
       <Routes>
