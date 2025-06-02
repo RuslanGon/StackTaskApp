6f4d469291799e0709458d0795bde38d219fd179
RuslanGon <ruslan441006@gmail.com>
2025-06-02 18:03:14 +0300

    wer

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -11,7 +11,17 @@ useEffect(() => {
     user && navigate('/dashboard')
 }, [navigate, user])
   return (
-  <div>Login</div>
+  <div className='w-full min-h-screen flex items-center justify-center flex-col lg:flex-row 
+  bg-[#f3f4f6]'>
+<div className='w-full md:w-auto flex gap-0 md:gap-40 flex-col md:flex-row items-center justify-center'>
+<div className='h-full w-full lg:w-2/3 flex flex-col items-center justify-center'>
+  <div className='w-full md:max-w-lg 2xl:max-w-3xl flex flex-col items-center justify-center gap-5 md:gap-y-10 2xl:-mt-20'>
+    <span className='flex gap-1 py-1 px-3 border rounded-full text-sm md:text-base border-gray-300 text-gray-600'>Manage your task in one place!</span>
+  </div>
+</div>
+<div></div>
+</div>
+  </div>
   )
 }
 
