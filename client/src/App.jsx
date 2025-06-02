8c8a295dde5138ca0abcd60a6165d4815eb3b12b
RuslanGon <ruslan441006@gmail.com>
2025-06-02 11:46:06 +0300

    wer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,26 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login.jsx';
 import Layout from './pages/Layout.jsx';
-import Dashboard from './pages/Dashboard.jsx'; // Предположим, что есть такая страница
+import Dashboard from './pages/Dashboard.jsx';
+import Tasks from './pages/Tasks.jsx';
+import Users from './pages/Users.jsx';
+import Trash from './pages/Trash.jsx';
+import TaskDetails from './pages/TaskDetails.jsx';
 
 function App() {
   return (
     <main className="w-full min-h-screen bg-[#f3f4f6]">
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Navigate to="/dashboard" />} />
+          <Route path='/' element={<Navigate to="/dashboard" />} />
+          <Route path='/dashboard' element={<Dashboard />}></Route>
+          <Route path='/tasks' element={<Tasks />}></Route>
+          <Route path='/completed/:status' element={<Tasks />}></Route>
+          <Route path='/in-progress/:status' element={<Tasks />}></Route>
+          <Route path='/todo/:status' element={<Tasks />}></Route>
+          <Route path='/team' element={<Users/>}></Route>
+          <Route path='/trashed' element={<Trash/>}></Route>
+          <Route path='/task/:id' element={<TaskDetails/>}></Route>
         </Route>
       </Routes>
     </main>
