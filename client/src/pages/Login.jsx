bec5e4b5c3b2dfa7b49f35337be5f8b7d1758ce4
RuslanGon <ruslan441006@gmail.com>
2025-06-02 23:35:14 +0300

    wer

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -69,6 +69,7 @@ useEffect(() => {
               error={errors.password ? errors.password.message : ''}
               />
               <span className='text-sm text-gray-500 hover:text-blue-600 hover:underline cursor-pointer'>Forget Password?</span>
+              <Button type='submit' label='Submit' className='w-full h-10 bg-blue-700 text-white' />
             </div>
           </form>
         </div>
