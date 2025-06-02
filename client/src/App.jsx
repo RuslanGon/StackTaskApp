7bd5d765bcd9b0ea5aa449ae6fcf088cac062a7f
RuslanGon <ruslan441006@gmail.com>
2025-06-02 11:32:25 +0300

    wer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,19 @@
-
-import './App.css'
+import { Navigate, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Login from './pages/Login.jsx';
+import Layout from './pages/Layout.jsx';
+import Dashboard from './pages/Dashboard.jsx'; // Предположим, что есть такая страница
 
 function App() {
-
   return (
-    <h1 class="text-3xl font-bold ">
-    Hello world!
-  </h1>
-  )
+    <main className="w-full min-h-screen bg-[#f3f4f6]">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Navigate to="/dashboard" />} />
+        </Route>
+      </Routes>
+    </main>
+  );
 }
 
-export default App
+export default App;
