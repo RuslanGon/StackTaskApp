d7a5117b2b6f399d0ddb5e9da129bcd3ad813be9
RuslanGon <ruslan441006@gmail.com>
2025-06-04 15:59:36 +0300

    wer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,9 +10,8 @@ import { Toaster } from 'sonner';
 import {  useDispatch, useSelector } from "react-redux";
 import Sidebar from "./components/Sidebar.jsx";
 import Navbar from "./components/Navbar.jsx";
-import { Fragment, useRef } from "react";
+import { Fragment } from "react";
 import { setOpenSidebar } from "./redux/slices/authSlice.js";
-import clsx from "clsx";
 import { Transition } from "@headlessui/react";
 import { IoClose } from "react-icons/io5";
 
@@ -44,56 +43,43 @@ function Layout() {
 }  
 
 const MobileSidebar = () => {
-  const  isSidebarOpen  = useSelector((state) => state.auth.isSidebarOpen);
-  const mobileMenuRef = useRef(null);
+  const isSidebarOpen = useSelector((state) => state.auth.isSidebarOpen);
   const dispatch = useDispatch();
 
   const closeSidebar = () => {
     dispatch(setOpenSidebar(false));
-  }
+  };
 
   return (
-    <>
-      <Transition
-        show={isSidebarOpen}
-        as={Fragment}
-        enter='transition-opacity duration-700'
-        enterFrom='opacity-x-10'
-        enterTo='opacity-x-100'
-        leave='transition-opacity duration-700'
-        leaveFrom='opacity-x-100'
-        leaveTo='opacity-x-0'
-      >
-        {(ref) => (
-          <div
-            ref={(node) => (mobileMenuRef.current = node)}
-            className={clsx(
-              "md:hidden w-full h-full bg-black/40 transition-all duration-700 transform ",
-              isSidebarOpen ? "translate-x-0" : "translate-x-full"
-            )}
-            onClick={() => closeSidebar()}
-          >
-            <div className='bg-white w-3/4 h-full'>
-              <div className='w-full flex justify-end px-5 mt-5'>
-                <button
-                  onClick={() => closeSidebar()}
-                  className='flex justify-end items-end'
-                >
-                  <IoClose size={25} />
-                </button>
-              </div>
-
-              <div className='-mt-10'>
-                <Sidebar />
-              </div>
-            </div>
+    <Transition
+      show={isSidebarOpen}
+      as={Fragment}
+      enter="transition-opacity duration-300"
+      enterFrom="opacity-0"
+      enterTo="opacity-100"
+      leave="transition-opacity duration-200"
+      leaveFrom="opacity-100"
+      leaveTo="opacity-0"
+    >
+      <div className="fixed inset-0 z-50 flex md:hidden">
+        <div
+          className="fixed inset-0 bg-black/40"
+          onClick={closeSidebar}
+        />
+        <div className="relative bg-white w-3/4 h-full shadow-lg z-50">
+          <div className="w-full flex justify-end px-4 mt-4">
+            <button onClick={closeSidebar} aria-label="Close sidebar">
+              <IoClose size={24} />
+            </button>
           </div>
-        )}
-      </Transition>
-    </>
+          <div className="-mt-6">
+            <Sidebar />
+          </div>
+        </div>
+      </div>
+    </Transition>
   );
-
-}
+};
 
   return (
     <main className="w-full min-h-screen bg-[#f3f4f6]">
