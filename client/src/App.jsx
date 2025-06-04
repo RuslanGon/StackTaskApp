7946e2d01934666374949d235004aabfe12d5b18
RuslanGon <ruslan441006@gmail.com>
2025-06-04 15:57:45 +0300

    wer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -55,43 +55,41 @@ const MobileSidebar = () => {
   return (
     <>
       <Transition
-  show={isSidebarOpen}
-  as={Fragment}
-  enter="transition-opacity duration-700"
-  enterFrom="opacity-0"
-  enterTo="opacity-100"
-  leave="transition-opacity duration-700"
-  leaveFrom="opacity-100"
-  leaveTo="opacity-0"
->
-  <div
-    ref={mobileMenuRef}
-    className={clsx(
-      "fixed inset-0 z-50 md:hidden flex transition-all duration-700 transform",
-      isSidebarOpen ? "translate-x-0" : "translate-x-full"
-    )}
-    onClick={closeSidebar}
-  >
-    <div
-      className="bg-white w-3/4 h-full"
-      onClick={(e) => e.stopPropagation()}
-    >
-      <div className="w-full flex justify-end px-5 mt-5">
-        <button
-          onClick={closeSidebar}
-          className="flex justify-end items-end"
-        >
-          <IoClose size={25} />
-        </button>
-      </div>
+        show={isSidebarOpen}
+        as={Fragment}
+        enter='transition-opacity duration-700'
+        enterFrom='opacity-x-10'
+        enterTo='opacity-x-100'
+        leave='transition-opacity duration-700'
+        leaveFrom='opacity-x-100'
+        leaveTo='opacity-x-0'
+      >
+        {(ref) => (
+          <div
+            ref={(node) => (mobileMenuRef.current = node)}
+            className={clsx(
+              "md:hidden w-full h-full bg-black/40 transition-all duration-700 transform ",
+              isSidebarOpen ? "translate-x-0" : "translate-x-full"
+            )}
+            onClick={() => closeSidebar()}
+          >
+            <div className='bg-white w-3/4 h-full'>
+              <div className='w-full flex justify-end px-5 mt-5'>
+                <button
+                  onClick={() => closeSidebar()}
+                  className='flex justify-end items-end'
+                >
+                  <IoClose size={25} />
+                </button>
+              </div>
 
-      <div className="-mt-10">
-        <Sidebar />
-      </div>
-    </div>
-    <div className="flex-1 bg-black/40" />
-  </div>
-</Transition>
+              <div className='-mt-10'>
+                <Sidebar />
+              </div>
+            </div>
+          </div>
+        )}
+      </Transition>
     </>
   );
 
