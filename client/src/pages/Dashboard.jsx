211ade94f03e6cd2515c1864f35247f5df152977
RuslanGon <ruslan441006@gmail.com>
2025-06-10 17:37:05 +0300

    wre

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import {
+  MdAdminPanelSettings,
+  MdKeyboardArrowDown,
+  MdKeyboardArrowUp,
+  MdKeyboardDoubleArrowUp,
+} from "react-icons/md";
+import { LuClipboardEdit } from "react-icons/lu";
+import { FaNewspaper, FaUsers } from "react-icons/fa";
+import { FaArrowsToDot } from "react-icons/fa6";
+import moment from "moment";
+
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div className='h-full py-4'>
+      <div className='grid grid-col-1 md:grid-cols-4'>
+
+      </div>
+    </div>
   )
 }
 
