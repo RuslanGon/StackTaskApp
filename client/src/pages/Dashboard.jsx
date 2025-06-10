bd855b5fe341cd96d78cf55a066f378ede16d1cf
RuslanGon <ruslan441006@gmail.com>
2025-06-10 17:46:09 +0300

    wre

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -9,12 +9,45 @@ import { LuClipboardEdit } from "react-icons/lu";
 import { FaNewspaper, FaUsers } from "react-icons/fa";
 import { FaArrowsToDot } from "react-icons/fa6";
 import moment from "moment";
+import {summary} from '../assets/data.js'
 
 
 const Dashboard = () => {
+
+  const stats = [
+    {
+      _id: "1",
+      label: "TOTAL TASK",
+      total: summary?.totalTasks || 0,
+      icon: <FaNewspaper />,
+      bg: "bg-[#1d4ed8]",
+    },
+    {
+      _id: "2",
+      label: "COMPLTED TASK",
+      total: totals["completed"] || 0,
+      icon: <MdAdminPanelSettings />,
+      bg: "bg-[#0f766e]",
+    },
+    {
+      _id: "3",
+      label: "TASK IN PROGRESS ",
+      total: totals["in progress"] || 0,
+      icon: <LuClipboardEdit />,
+      bg: "bg-[#f59e0b]",
+    },
+    {
+      _id: "4",
+      label: "TODOS",
+      total: totals["todo"],
+      icon: <FaArrowsToDot />,
+      bg: "bg-[#be185d]" || 0,
+    },
+  ]; 
+
   return (
     <div className='h-full py-4'>
-      <div className='grid grid-col-1 md:grid-cols-4'>
+      <div className='grid grid-cols-1 md:grid-cols-4'>
 
       </div>
     </div>
