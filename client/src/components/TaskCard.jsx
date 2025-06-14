09757ac7ce5c21c3068ec02ac8ac7e5901c27de7
RuslanGon <ruslan441006@gmail.com>
2025-06-14 13:10:15 +0300

    wer

--- client/src/components/TaskCard.jsx
+++ client/src/components/TaskCard.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import { MdKeyboardArrowDown, MdKeyboardArrowUp, MdKeyboardDoubleArrowUp } from 'react-icons/md';
 import { useSelector } from 'react-redux';
+import { PRIOTITYSTYELS } from '../utils/index.js';
+import clsx from 'clsx';
 
 const ICONS = {
     high: <MdKeyboardDoubleArrowUp />,
@@ -9,15 +11,26 @@ const ICONS = {
   };
 
 const TaskCard = ({task}) => {
-
-    const { user } = useSelector((state) => state.auth);
-    const [open, setOpen] = useState(false);    
+  const { user } = useSelector((state) => state.auth);
+  const [open, setOpen] = useState(false);
 
   return (
-<>
-
-</>
-  )
+    <>
+      <div className="w-full h-fit bg-white shadow-md p-4 rounded">
+        <div className='w-full flex justify-between'>
+        <div
+            className={clsx(
+              "flex flex-1 gap-1 items-center text-sm font-medium",
+              PRIOTITYSTYELS[task?.priority]
+            )}
+          >
+            <span className='text-lg'>{ICONS[task?.priority]}</span>
+            <span className='uppercase'>{task?.priority} Priority</span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default TaskCard
