f122d839344dd5b13492104a9693cbafda98bc2a
RuslanGon <ruslan441006@gmail.com>
2025-06-18 13:15:23 +0300

    wer

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -6,6 +6,7 @@ const Button = ({ className, label, type, onClick = () => {}, icon }) => {
       type={type || "button"}
       className={clsx("px-3 py-2 outline-none rounded cursor-pointer", className)}
       onClick={onClick}
+      
     >
       <span>{label}</span>
 
