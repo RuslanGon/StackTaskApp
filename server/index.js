1eaba0306df1e884aa0dbdd07761ef3e67e0d268
RuslanGon <ruslan441006@gmail.com>
2025-06-22 13:24:45 +0300

    wrr

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded())
+app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
 app.use(morgan('dev'))
 // app.use('/api')
