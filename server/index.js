842e05e47ecf01e4bb5774d6f96af485590c6d3d
RuslanGon <ruslan441006@gmail.com>
2025-06-22 13:17:09 +0300

    wer

--- server/index.js
+++ server/index.js
@@ -3,11 +3,20 @@ import startServer from './db.js';
 import cors from 'cors'
 import cookieParser from 'cookie-parser';
 import morgan from 'morgan'
+import { errorHandler, routeNotFound } from './middlewares/errorMiddleware.js';
 
 
 const app = express();
-app.use(express.json())
+
+
 app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(cookieParser())
+app.use(morgan('dev'))
+// app.use('/api')
+app.use(routeNotFound)
+app.use(errorHandler)
 
 
 startServer(app);
