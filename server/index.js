1b694f79106706dfa6d6e26d4113418986c8e690
RuslanGon <ruslan441006@gmail.com>
2025-06-22 12:49:53 +0300

    wer

--- server/index.js
+++ server/index.js
@@ -1,17 +1,13 @@
 import express from 'express';
-import dotenv from 'dotenv';
 import startServer from './db.js';
 import cors from 'cors'
 import cookieParser from 'cookie-parser';
+import morgan from 'morgan'
 
-
-dotenv.config(); 
 
 const app = express();
 app.use(express.json())
 app.use(cors())
-
-
 
 
 startServer(app);
